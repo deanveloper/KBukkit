ef8a93d384be5046423cf2e7a5341a0ebb81c52b
Dean B <deanvelopermn@gmail.com>
2016-04-12 10:48:36 -0500

    Shortened methods

--- src/main/kotlin/com/deanveloper/kbukkit/CustomPlayer.kt
+++ src/main/kotlin/com/deanveloper/kbukkit/CustomPlayer.kt
@@ -13,30 +13,19 @@ open class CustomPlayer private constructor(player: Player) : Player by player {
     init {
         byId.put(uniqueId, this);
         byName.put(name, this);
+
+        KBukkitRunnable {
+            byId.remove(uniqueId);
+            byName.remove(name);
+        }.runTaskLater(KBukkitPlugin.instance, 20L);
     }
 
     companion object {
         private val byId: MutableMap<UUID, CustomPlayer> = mutableMapOf();
         private val byName: MutableMap<String, CustomPlayer> = mutableMapOf();
 
-        operator fun get(index: UUID): CustomPlayer {
-            val value = byId[index];
-            if (value != null) {
-                return value;
-            }
+        operator fun get(index: UUID): CustomPlayer = byId[index] ?: CustomPlayer(Bukkit.getPlayer(index)!!);
 
-            val p = Bukkit.getPlayer(index);
-            return CustomPlayer(p!!);
-        }
-
-        operator fun get(index: String): CustomPlayer {
-            val value = byName[index];
-            if (value != null) {
-                return value;
-            }
-
-            val p = Bukkit.getPlayer(index);
-            return CustomPlayer(p!!);
-        }
+        operator fun get(index: String): CustomPlayer = byName[index] ?: CustomPlayer(Bukkit.getPlayer(index)!!);
     }
 }

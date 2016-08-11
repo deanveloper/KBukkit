46f85cc155ed43cdd8309ea7a27194c992247e2b
Dean B <deanvelopermn@gmail.com>
2016-08-11 03:01:23 -0500

    Added ChatColorUtil to make ChatColors easier to use

---
+++ src/main/kotlin/com/deanveloper/kbukkit/ChatColorUtil.kt
@@ -0,0 +1,16 @@
+package com.deanveloper.kbukkit
+
+import org.bukkit.ChatColor as BukkitColor
+import net.md_5.bungee.api.ChatColor as BungeeColor
+
+/**
+ * Adding a color to a string
+ *
+ * @author Dean
+ */
+operator fun BukkitColor.plus(s: String): String = this.toString() + s
+operator fun BungeeColor.plus(s: String): String = this.toString() + s
+operator fun BukkitColor.plus(color: BukkitColor): String = this.toString() + color
+operator fun BungeeColor.plus(color: BukkitColor): String = this.toString() + color
+operator fun BukkitColor.plus(color: BungeeColor): String = this.toString() + color
+operator fun BungeeColor.plus(color: BungeeColor): String = this.toString() + color

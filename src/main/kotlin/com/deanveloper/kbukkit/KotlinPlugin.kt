01a3dc70fdaebd4abf4f1615cf2970af727d9d38
Dean Bassett <deanvelopermn@gmail.com>
2017-01-19 16:54:37 -0500

    Optimized imports

--- src/main/kotlin/com/deanveloper/kbukkit/KotlinPlugin.kt
+++ src/main/kotlin/com/deanveloper/kbukkit/KotlinPlugin.kt
@@ -1,26 +1,10 @@
 package com.deanveloper.kbukkit
 
-import org.bukkit.Server
-import org.bukkit.plugin.PluginDescriptionFile
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.plugin.java.JavaPluginLoader
 
 /**
  * Mainly just here for convenience. Looks *way* cooler to use a KotlinPlugin over a JavaPlugin!
  *
  * @author Dean B <dean@deanveloper.com>
  */
-abstract class KotlinPlugin : JavaPlugin {
-
-    constructor() : super()
-
-    /**
-     * Used for tests
-     */
-    internal constructor(server: Server) : super(
-            JavaPluginLoader(server),
-            PluginDescriptionFile("KBukkit", "test", KBukkitPlugin::class.qualifiedName),
-            null,
-            null
-    )
-}
+abstract class KotlinPlugin() : JavaPlugin()
